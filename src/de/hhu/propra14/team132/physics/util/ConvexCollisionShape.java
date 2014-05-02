001b0355bbbe87efc68e19e85a713a98e595b946
Chris Rutenkolk <C.Rutenkolk93@gmx.de>
2014-05-03 00:17:27 +0200

    adapting the SAT-like collision I already wrote some time ago

--- src/de/hhu/propra14/team132/physics/util/ConvexCollisionShape.java
+++ src/de/hhu/propra14/team132/physics/util/ConvexCollisionShape.java
@@ -3,7 +3,6 @@ package de.hhu.propra14.team132.physics.util;
 import java.awt.Polygon;
 
 
-
 public class ConvexCollisionShape {
 	
 //probably really nasty class with a lot of attributes and a lot of algorithms for collision. Think thrice before changing!
@@ -63,24 +62,197 @@ public class ConvexCollisionShape {
 	}
 	
 	
-	
-	private Vector2D[] getCollisionAxes() {
-		return this.collisionAxes;
+	public static boolean PolygonVsPolygonHitTest(ConvexCollisionShape shape1,ConvexCollisionShape shape2){
+		//on y axis
+		if(shape1.getMinOnY()>shape2.getMaxOnY()||shape1.getMaxOnY()<shape2.getMinOnY()) return false;
+		//on x axis
+		//IF the min of ax is greater than the max of bx, [Logical] OR the max of ax is smaller than the min of bx, THEN...
+		if(shape1.getMinOnX()>shape2.getMaxOnX()||shape1.getMaxOnX()<shape2.getMinOnX()) return false;
+		
+		Vector2D[] axes1 = shape1.getCollisionAxes();
+		Vector2D[] axes2 = shape2.getCollisionAxes();
+		//Vector2D[] axes = combineAxisArrays(shape1.getCollisionAxes(), shape2.getCollisionAxes());
+		//axes = excludeParallelAxes(axes);
+		//check for parallel axes and replace
+		// loop over the axes
+		for (int i = 0; i < axes1.length; i++) {
+		    Vector2D axisToTest = axes1[i];
+		    // project both shapes onto the axis
+		    // x value represents min projected vertex on axis and y represents the max projected vertex on the axis. It's just that I need a wrapper at this point...
+		    Vector2D p1 = shape1.projectOnAxis(axisToTest);
+		    Vector2D p2 = shape2.projectOnAxis(axisToTest);
+		    // do the projections overlap on this axis?
+		    //IF the min of p1 is greater than the max of p2, [Logical] OR the max of p1 is smaller than the min of p2, THEN...
+		    if ((p1.getX()>p2.getY()||p1.getY()<p2.getX())) {  
+		   // then we can guarantee that the shapes do not overlap
+		     return false;
+		    }
+		}
+		for (int i = 0; i < axes2.length; i++) {
+		    Vector2D axisToTest = axes2[i];
+		    // project both shapes onto the axis
+		    // x value represents min projected vertex on axis and y represents the max projected vertex on the axis. It's just that I need a wrapper at this point...
+		    Vector2D p1 = shape1.projectOnAxis(axisToTest);
+		    Vector2D p2 = shape2.projectOnAxis(axisToTest);
+		    // do the projections overlap on this axis?
+		    //IF the min of p1 is greater than the max of p2, [Logical] OR the max of p1 is smaller than the min of p2, THEN...
+		    if ((p1.getX()>p2.getY()||p1.getY()<p2.getX())) {  
+		   // then we can guarantee that the shapes do not overlap
+		     return false;
+		    }
+		}
+		// if we get here then we know that every axis had overlap on it
+		// so we can guarantee an intersection
+		return true;
 	}
+	public static Vector2D PolygonVsPolygonGetMtv(ConvexCollisionShape shape1,ConvexCollisionShape shape2){
+			//on y axis
+			if(shape1.getMinOnY()>shape2.getMaxOnY()||shape1.getMaxOnY()<shape2.getMinOnY()) return null;
+			//on x axis
+			//IF the min of ax is greater than the max of bx, [Logical] OR the max of ax is smaller than the min of bx, THEN...
+			if(shape1.getMinOnX()>shape2.getMaxOnX()||shape1.getMaxOnX()<shape2.getMinOnX()) return null;
+			
+			
+		double overlap=Double.MAX_VALUE;//damn large value because we want to check if the found overlap is smaller, so I'm using the satic value double.MAX_VALUE here :D
+		Vector2D smallestOverlapAxis=new Vector2D();
+		Vector2D[] axes1 = shape1.getCollisionAxes();
+		Vector2D[] axes2 = shape2.getCollisionAxes();
+		//check for parallel axes and replace
+		//axes2=excludeParallelAxes(axes1, axes2);
+		// loop over the axes1
+		for (int i = 0; i < axes1.length; i++) {
+		  Vector2D axisToTest = axes1[i];
+		  // project both shapes onto the axis
+		  // x value represents min projected vertex on axis and y represents the max projected vertex on the axis. It's just that I need a wrapper at this point...
+		  Vector2D p1 = shape1.projectOnAxis(axisToTest);
+		  Vector2D p2 = shape2.projectOnAxis(axisToTest);
+		  // do the projections overlap on this axis?
+		  //IF the min of p1 is greater than the max of p2, [Logical] OR the max of p1 is smaller than the min of p2, THEN...
+		  if ((p1.getX()>p2.getY()||p1.getY()<p2.getX())) {  
+			// then we can guarantee that the shapes do not overlap
+			  return null;
+			  
+		  }
+		  else{
+			  // so we DO have an overlap on that axis. the overlap and the projection together make a potential mtv. Calc it and save it.
+			  if(p2.getY()>p1.getY()){
+			      if(p1.getY()-p2.getX()<overlap){//mhmhmhm???
+			        overlap=p1.getY()-p2.getX();
+			        smallestOverlapAxis=axisToTest;
+			       }
+			     }
+			     else{
+			      if(p2.getY()-p1.getX()<overlap){//mhmhmhm???
+			       overlap=p2.getY()-p1.getX();
+			       smallestOverlapAxis=axisToTest;
+			      }
+			     }
+			  
+		  }
+		}
+		// loop over the axes2
+		for (int i = 0; i < axes2.length; i++) {
+		  Vector2D axisToTest = axes2[i];
+		  // project both shapes onto the axis
+		  Vector2D p1 = shape1.projectOnAxis(axisToTest);
+		  Vector2D p2 = shape2.projectOnAxis(axisToTest);
+		  // do the projections overlap?
+		  if ((p1.getX()>p2.getY()||p1.getY()<p2.getX())) {
+		    // then we can guarantee that the shapes do not overlap
+			  return null;
+		  }
+		  else{
+			  // so we DO have an overlap on that axis. the overlap and the projection together make a potential mtv. Calc it and save it.
+			  if(p2.getY()>p1.getY()){
+			      if(p1.getY()-p2.getX()<overlap){//mhmhmhm???
+			        overlap=p1.getY()-p2.getX();
+			        smallestOverlapAxis=axisToTest;
+			       }
+			     }
+			     else{
+			      if(p2.getY()-p1.getX()<overlap){//mhmhmhm???
+			       overlap=p2.getY()-p1.getX();
+			       smallestOverlapAxis=axisToTest;
+			      }
+			     }
+			  
+		  }
+		}
+		if(overlap==0)return null;
+		smallestOverlapAxis.multiplyWith(overlap);//make it the mtv
+		return smallestOverlapAxis;
+
+
+	}
+	
+	private Vector2D projectOnAxis(Vector2D axisToTest) {
+		//axisToTest.multiplyWith((double)1/axisToTest.getLength());//normalize... // no, axes will be normalized when initialized.
+		//NEVER use this method with not-normalized axes! See the comment above and change it if you want. This is important so I will just leave this here for the time.
+		
+		//the vertexes are seen as Vectors on their own. The minimum and the maximum
+		double min = axisToTest.getX()*xPoints[0]+axisToTest.getY()*yPoints[0];
+		double max = min;
+		for (int i = 0; i < this.xPoints.length; i++) {
+		  //normalize to get accurate projections!!! I could just assume this, or, assert it with additional computing. 
+		  //I hope no one will ever touch this as soon as it's finished. I do not assert the normalisation for now!! 
+		  //take care if you want to change this/related code!
+			double p = axisToTest.getX()*xPoints[i]+axisToTest.getY()*yPoints[i];
+			if (p < min) {// AAAA! possible negative numbers ?? OR??? not sure TODO:examine if negative numbers occure or if the are bad.
+		    min = p;
+		  } else if (p > max) {
+		    max = p;
+		  }
+		}
+		Vector2D proj = new Vector2D(min, max);
+		return proj;
+	}
+
 	public void computeCollisionAxes(){
 		Vector2D[] axes=new Vector2D[xPoints.length];
 		for(int i=0;i<this.xPoints.length;i++){
 			//obtaining the edge: vectorb-vectora
-			// edge is an relative Vector, describing the distance between the two vertices. It does NOT describe a Point on its own!
+			//edge is an relative Vector, describing the distance between the two vertices. It does NOT describe a Point on its own!
 			//Vector2D edge = new Vector2D(xPoints[(i+1)%xPoints.length]-xPoints[i],yPoints[(i+1)%yPoints.length]-yPoints[i]);
 			//Vector2D axis = edge.getLeftHandNormal();
 			Vector2D axis = new Vector2D(yPoints[(i+1)%yPoints.length]-yPoints[i],-(xPoints[(i+1)%xPoints.length]-xPoints[i]));
-			//again:the axis should be normalized to get accurate projections!!!
+			//again:the axis should be normalized to get accurate projections!!! better do that here.
 			axis.multiplyWith(1/axis.getLength());
 			axes[i]=axis;
 		}
-//		this.collisionAxes = excludeParallelAxes(axes); TODO: implement excludeParallelAxes
+		this.collisionAxes = excludeParallelAxes(axes); 
 	}
+	
+	public static Vector2D[] excludeParallelAxes(Vector2D[] oldAxes){
+		int parallelCount=0;
+		int[] badAxes=new int[oldAxes.length];
+		//mark the parallelAxes, store their position in the array.
+		for(int i=0;i<oldAxes.length;i++){
+			for(int j=0;j>i&&j<oldAxes.length;i++){
+				if(oldAxes[i].isParallel(oldAxes[j])){
+					badAxes[parallelCount]=j;
+					parallelCount++;
+				}
+			}
+		}
+		//now remove all the parallel axes, if there are any.
+		if(parallelCount>0){
+			//doing it this way is most probably faster, even though it looks disgustingly overcomplicated.
+			Vector2D[] newAxes= new Vector2D[oldAxes.length-parallelCount];
+		
+			int j=0;
+			for(int i=0;j<parallelCount;){
+				if(i!=badAxes[j]){
+					newAxes[i-j]=oldAxes[i];
+				}
+				else{
+					j++;
+				}
+			}
+			return newAxes;
+		}
+		return oldAxes;
+	}
+	
 	public Vector2D[] getComputedCollisionAxes(){
 		Vector2D[] axes=new Vector2D[xPoints.length-1];
 		double[] slopes= new double[xPoints.length-1];
@@ -96,6 +268,7 @@ public class ConvexCollisionShape {
 		return axes;
 	}
 	
+
 	
 	public double[] getOnX(){
 		double[] minMax={
@@ -151,6 +324,11 @@ public class ConvexCollisionShape {
 		}
 		return y;
 	}
+	
+	private Vector2D[] getCollisionAxes() {
+		return this.collisionAxes;
+	}
+	
 	public void setyPoints(double[] yPoints) {
 		this.yPoints = yPoints;
 	}
