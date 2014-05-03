c29e57e900af7db75300d83e51efd224daa102f1
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2014-05-03 21:50:35 +0200

    Weapons Buttons are now in an ArrayList and have a placeholder ActionListener

--- src/de/hhu/propra14/team132/GUI/WeaponsPanel.java
+++ src/de/hhu/propra14/team132/GUI/WeaponsPanel.java
@@ -1,33 +1,46 @@
 package de.hhu.propra14.team132.GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  * Created by fabian on 03.05.14.
  */
 public class WeaponsPanel extends JPanel {
     MainPanel mainPanel;
-    JButton weapon1;
-    JButton weapon2;
-    JButton weapon3;
-    JButton weapon4;
-    JButton weapon5;
+    ArrayList<JButton> weapons;
 
     public WeaponsPanel(MainPanel mainPanel) {
         this.mainPanel=mainPanel;
 
-        weapon1=new JButton("Bazooka");
-        weapon2=new JButton("Grenade");
-        weapon3=new JButton("Mine");
-        weapon4=new JButton("Punch");
-        weapon5=new JButton("Super sheep");
+        weapons=new ArrayList<JButton>();
 
-        this.add(weapon1);
-        this.add(weapon2);
-        this.add(weapon3);
-        this.add(weapon4);
-        this.add(weapon5);
+        weapons.add(new JButton("Bazooka"));
+        weapons.get(0).addActionListener(new WeaponsListener());
+        weapons.add(new JButton("Grenade"));
+        weapons.get(1).addActionListener(new WeaponsListener());
+        weapons.add(new JButton("Mine"));
+        weapons.get(2).addActionListener(new WeaponsListener());
+        weapons.add(new JButton("Super sheep"));
+        weapons.get(3).addActionListener(new WeaponsListener());
+        weapons.add(new JButton("Punch"));
+        weapons.get(4).addActionListener(new WeaponsListener());
+
+        this.add(weapons.get(0));
+        this.add(weapons.get(1));
+        this.add(weapons.get(2));
+        this.add(weapons.get(3));
+        this.add(weapons.get(4));
 
         this.setVisible(false);
+    }
+
+    class WeaponsListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            String pressed=((JButton)e.getSource()).getText();
+        }
     }
 }
