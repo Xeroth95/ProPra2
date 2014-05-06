9437ddcac937d18e2a5870425cbb64fb40ee6a53
Isabel Wingen <Isabel.Wingen@hhu.de>
2014-05-06 17:23:20 +0200

    added Message and MessageType

---
+++ src/de/hhu/propra14/team132/gameSystem/Message.java
@@ -0,0 +1,11 @@
+package de.hhu.propra14.team132.gameSystem;
+
+/**
+ * Created by isabel on 06.05.14.
+ */
+public class Message {
+    //with Message-Object one can exchange messages between the Objects
+
+    public Message() {
+    }
+}
---
+++ src/de/hhu/propra14/team132/gameSystem/MessageType.java
@@ -0,0 +1,9 @@
+package de.hhu.propra14.team132.gameSystem;
+
+/**
+ * Created by isabel on 06.05.14.
+ */
+public enum MessageType {
+    //this is the enum for MessageType. Here we will list all the different MessageTypes
+    
+}
