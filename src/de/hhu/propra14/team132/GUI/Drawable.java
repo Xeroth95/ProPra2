d6681fad84e43f534b525a809565b6ec8f38f0eb
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2014-05-06 14:32:56 +0200

    added interface 'Drawable'

---
+++ src/de/hhu/propra14/team132/GUI/Drawable.java
@@ -0,0 +1,12 @@
+package de.hhu.propra14.team132.GUI;
+
+import java.awt.*;
+
+/**
+ * Created by fabian on 06.05.14.
+ */
+public abstract interface Drawable {
+    //every object that wants to be drawn on the GamePanel has to implement this interface
+
+    public abstract void draw(Graphics g);
+}
