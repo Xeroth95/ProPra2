3377a786b65f27309972cceaf06414c5cde173b0
Isabel Wingen <Isabel.Wingen@hhu.de>
2014-05-06 17:15:33 +0200

    some changes on the GameManager

--- src/de/hhu/propra14/team132/gameSystem/GameManager.java
+++ src/de/hhu/propra14/team132/gameSystem/GameManager.java
@@ -1,17 +1,25 @@
 package de.hhu.propra14.team132.gameSystem;
 
 import de.hhu.propra14.team132.GUI.MainFrame;
+import de.hhu.propra14.team132.gameMechanics.Map;
+import de.hhu.propra14.team132.gameObjects.Terrain;
 
 /**
  * Created by isabel on 06.05.14.
  */
 public class GameManager {
+
     public GameManager() {
 
     }
 
     public static void main(String[] args) {
         MainFrame frm=new MainFrame();
+        //die Befehle der GUI abwarten
+        //Spiel starten
+        //Karte erstellen
+        //Würmer drauf setzen
+        //
     }
 
 }
