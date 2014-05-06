f8d90ed604af25d764880d5ce1c2f024c36d139d
Isabel Wingen <Isabel.Wingen@hhu.de>
2014-05-06 14:30:37 +0200

    GameObject now extends CollsionObjects

--- src/de/hhu/propra14/team132/gameObjects/GameObject.java
+++ src/de/hhu/propra14/team132/gameObjects/GameObject.java
@@ -1,6 +1,8 @@
 package de.hhu.propra14.team132.gameObjects;
 
-public class GameObject {
+import de.hhu.propra14.team132.physics.CollisionObject;
+
+public abstract class GameObject extends CollisionObject{
     private String name;
     public GameObject(String name) {
         this.name=name;
