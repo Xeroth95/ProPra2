102bb61f3e41362dfc7a9da11f0eaa96ce5c2dce
Isabel Wingen <Isabel.Wingen@hhu.de>
2014-05-07 19:07:41 +0200

    added some comments

--- src/de/hhu/propra14/team132/gameSystem/Communicatable.java
+++ src/de/hhu/propra14/team132/gameSystem/Communicatable.java
@@ -3,7 +3,13 @@ package de.hhu.propra14.team132.gameSystem;
 /**
  * Created by isabel on 07.05.14.
  */
+
+/**
+ * this Interface must be implemented by all classes, which want to receive and send Messages. It is neccesary to have a
+ * Constructor with GameManager
+ */
 public interface Communicatable {
+
     public void receiveMessage(Message m);  //with this Method the Objects can receive Messages. It shoul contain
                                             //something to intepret the Message, that means it should get the type,
                                             //cast it to the right Object and get the things it needs to do what it should to
