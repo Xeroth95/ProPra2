fcdd7725a6827abbbf5c13b0d934baf101ff414b
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2014-05-09 15:57:21 +0200

    added option to set the length of a round to GameSettingsPanel

--- src/de/hhu/propra14/team132/GUI/GameSettingsPanel.java
+++ src/de/hhu/propra14/team132/GUI/GameSettingsPanel.java
@@ -12,22 +12,69 @@ public class GameSettingsPanel extends JPanel {
     //this panel contain all the settings that influence the GameMechanics
     MainPanel mainPanel;
 
-    JButton button_go_back;
+    JPanel timePanel;
+    JLabel timeLabel;
+    JTextField timeTextField;
+    JButton timeButtonPlus;
+    JButton timeButtonMinus;
+
+    JButton goBackButton;
 
     public GameSettingsPanel(MainPanel mainPanel) {
         this.mainPanel=mainPanel;
-        this.setLayout(new GridLayout(1,1,10,10));
+        this.setLayout(new GridLayout(2,1,10,10));
 
-        button_go_back=new JButton("Go back to Settings Menu");
-        button_go_back.addActionListener(new GoBackListener());
+        timePanel=new JPanel(new FlowLayout());
+        goBackButton=new JButton("Go back to Settings Menu");
+        goBackButton.addActionListener(new GoBackListener());
+        timeLabel=new JLabel("Roundlength in seconds:");
+        timeTextField=new JTextField(4);
+        timeTextField.setText("120");
+        timeTextField.setEditable(false);
+        timeButtonPlus=new JButton("+");
+        timeButtonPlus.addActionListener(new TimePlusListener());
+        timeButtonMinus=new JButton("-");
+        timeButtonMinus.addActionListener(new TimeMinusListener());
 
-        this.add(button_go_back);
+        timePanel.add(timeLabel);
+        timePanel.add(timeTextField);
+        timePanel.add(timeButtonPlus);
+        timePanel.add(timeButtonMinus);
+
+        this.add(timePanel);
+        this.add(goBackButton);
     }
 
     class GoBackListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             GameSettingsPanel.this.mainPanel.showPanel("3");//switch back to settings menu
+        }
+    }
+
+    class TimePlusListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int time=Integer.parseInt(GameSettingsPanel.this.timeTextField.getText());
+            if(time>=99999) {
+                GameSettingsPanel.this.timeTextField.setText(String.valueOf(99999));
+            }
+            else {
+                GameSettingsPanel.this.timeTextField.setText(String.valueOf(time + 1));
+            }
+        }
+    }
+
+    class TimeMinusListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int time=Integer.parseInt(GameSettingsPanel.this.timeTextField.getText());
+            if(time<=0) {
+                GameSettingsPanel.this.timeTextField.setText(String.valueOf(0));
+            }
+            else {
+                GameSettingsPanel.this.timeTextField.setText(String.valueOf(time - 1));
+            }
         }
     }
 }
