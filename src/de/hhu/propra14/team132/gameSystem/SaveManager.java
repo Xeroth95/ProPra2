20b97cef5c6d4fccffab68c93f0b965a0d5bc723
Isabel Wingen <Isabel.Wingen@hhu.de>
2014-05-17 13:52:48 +0200

    added SaveManager, but it is not used yet

--- src/de/hhu/propra14/team132/gameSystem/SaveManager.java
+++ src/de/hhu/propra14/team132/gameSystem/SaveManager.java
@@ -3,11 +3,20 @@ package de.hhu.propra14.team132.gameSystem;
 import com.google.gson.Gson;
 import de.hhu.propra14.team132.gameMechanics.Map;
 
+import java.io.FileWriter;
+
 /**
  * Created by isabel on 17.05.14.
  */
 public class SaveManager {
     public SaveManager(Map map, String path) {
-        Gson gson=new Gson();
+        try {
+            Gson gson = new Gson();
+            FileWriter fw = new FileWriter(path);
+            gson.toJson(map, fw);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
