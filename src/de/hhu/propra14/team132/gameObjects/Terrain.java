963e28cc09199acbd6128fb4e56964f7a0993c52
Isabel Wingen <Isabel.Wingen@hhu.de>
2014-05-18 18:58:39 +0200

    changed name of file

--- src/de/hhu/propra14/team132/gameObjects/Terrain.java
+++ src/de/hhu/propra14/team132/gameObjects/Terrain.java
@@ -31,7 +31,7 @@ public class Terrain extends GameObject {
     
     static{
     	try{
-    		textureImage= ImageIO.read(new File("res/img/textures/terrain2.png"));
+    		textureImage= ImageIO.read(new File("res/img/textures/terrain2.jpg"));
             texture=new TexturePaint(textureImage, new Rectangle(0,0,48,48));
     	}catch(IOException e){
     		System.err.println("could not load the Texture for the Terrain!");
