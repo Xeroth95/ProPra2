c0dda626a9da4ec6c31e993696a82a433e187bd0
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2014-05-24 15:57:05 +0200

    projectiles are drawn as black circles. Classes that extend Projectile can specify the way they want to be drawn

--- src/de/hhu/propra14/team132/gameObjects/Projectile.java
+++ src/de/hhu/propra14/team132/gameObjects/Projectile.java
@@ -1,11 +1,10 @@
 package de.hhu.propra14.team132.gameObjects;
 
-import de.hhu.propra14.team132.gameSystem.GameManager;
+import de.hhu.propra14.team132.GUI.Drawable;
 import de.hhu.propra14.team132.physics.CollisionMode;
 import de.hhu.propra14.team132.physics.CollisionObject;
 import de.hhu.propra14.team132.physics.Effect;
 import de.hhu.propra14.team132.physics.Gravity;
-import de.hhu.propra14.team132.physics.util.ConvexCollisionShape;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,7 +13,7 @@ import java.util.ArrayList;
 /**
  * Created by isabel on 06.05.14.
  */
-public abstract class Projectile extends GameObject{
+public abstract class Projectile extends GameObject implements Drawable{
     public Projectile(int teamID) {
         super(teamID);
     }
@@ -42,7 +41,8 @@ public abstract class Projectile extends GameObject{
     //Drawable Methods:
     @Override
     public void draw(Graphics2D g2d, JPanel p) {
-
+        g2d.setColor(Color.BLACK);
+        g2d.drawOval((int)this.getPosition().getX(),(int)this.getPosition().getY(),10,10);
     }
 
 	@Override
