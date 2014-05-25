8d2ff40d41c2a85d83c7edc8b692c519372b9f45
Chris Rutenkolk <C.Rutenkolk93@gmx.de>
2014-05-25 16:11:21 +0200

    removed unnecessary constructors

--- src/de/hhu/propra14/team132/gameObjects/Weapons/Skip.java
+++ src/de/hhu/propra14/team132/gameObjects/Weapons/Skip.java
@@ -7,13 +7,7 @@ import de.hhu.propra14.team132.gameObjects.Projectile;
  * Created by isabel on 23.05.14.
  */
 public class Skip extends Weapon {
-    public Skip(int currentAmmo) {
-        super(currentAmmo);
-    }
-
-    public Skip(){ // actually implement a proper constructor instead of just doing nothing!
-    	super(); //Constructor for INFINITE_AMMO
-    }
+	
     @Override
     public void shoot() {
         //do something with the Projetile; todo: I do not know how to "shoot"
