64ac4af91f60249f81f9b5b6d49a7def327ba24c
Chris Rutenkolk <C.Rutenkolk93@gmx.de>
2014-05-26 01:08:30 +0200

    now after you win a game you get back to the main menu

--- src/de/hhu/propra14/team132/gameMechanics/rule/WinRule.java
+++ src/de/hhu/propra14/team132/gameMechanics/rule/WinRule.java
@@ -5,6 +5,7 @@ import de.hhu.propra14.team132.gameMechanics.Player;
 import de.hhu.propra14.team132.gameObjects.Worm;
 import de.hhu.propra14.team132.gameSystem.Message;
 import de.hhu.propra14.team132.gameSystem.MessageType;
+import de.hhu.propra14.team132.gameSystem.Messages.StopMessage;
 
 public class WinRule extends PassiveRule{
 
@@ -33,7 +34,8 @@ public class WinRule extends PassiveRule{
 			}
 			if(lost){
 				System.out.format("Player %s lost the game! Everyone else is a winner!\nHave a nice day ;);););)",p.playerID);
-				System.exit(0);
+				gameMap.getManager().sendMessage(new StopMessage());
+				gameMap.getManager().mainFrame.mainPanel.showPanel("1");
 			}
 		}
 	}
