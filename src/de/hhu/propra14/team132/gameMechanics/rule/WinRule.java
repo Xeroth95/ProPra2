88bf259e7bf58f6037f653d1a7e7504fd44391fe
Chris Rutenkolk <C.Rutenkolk93@gmx.de>
2014-05-26 01:02:22 +0200

    now there are losers and winners!

---
+++ src/de/hhu/propra14/team132/gameMechanics/rule/WinRule.java
@@ -0,0 +1,41 @@
+package de.hhu.propra14.team132.gameMechanics.rule;
+
+import de.hhu.propra14.team132.gameMechanics.Map;
+import de.hhu.propra14.team132.gameMechanics.Player;
+import de.hhu.propra14.team132.gameObjects.Worm;
+import de.hhu.propra14.team132.gameSystem.Message;
+import de.hhu.propra14.team132.gameSystem.MessageType;
+
+public class WinRule extends PassiveRule{
+
+	public WinRule(Map gameMap) {
+		super(gameMap);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void receiveMessage(Message m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected MessageType[] defineMessageTypesToReactTo() {
+		return null;
+	}
+
+	@Override
+	public void applyRule() {
+		for(Player p:gameMap.getPlayers()){
+			boolean lost=true;
+			for(Worm w:p.getWorms()){
+				if(!w.isMarkedForDeletion())lost=false;
+			}
+			if(lost){
+				System.out.format("Player %s lost the game! Everyone else is a winner!\nHave a nice day ;);););)",p.playerID);
+				System.exit(0);
+			}
+		}
+	}
+
+}
